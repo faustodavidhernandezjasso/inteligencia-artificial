ed6301e9ab4273b98f9d928068a54d9ed1672410
Fausto David Hernandez Jasso <fausto.david.hernandez.jasso@ciencias.unam.mx>
2023-09-15 23:33:18 -0600

    Adding DFS.

--- codigos/solving_problems_by_searching/algorithms.py
+++ codigos/solving_problems_by_searching/algorithms.py
@@ -2,6 +2,7 @@ from action import Action
 from search_problem import SearchProblem
 from node import Node
 from graph import Graph
+from queue import PriorityQueue
 
 class Algorithms:
 
@@ -21,8 +22,25 @@ class Algorithms:
             return node
         frontier = [node]
         reached = [problem.initial_state]
-        while len(frontier) != 0:
+        while frontier:
             n = frontier.pop(0)
+            for child in self.expand(problem, n):
+                s = child.state
+                if problem.goal_state == s:
+                    return child
+                if not(s in reached):
+                    reached.append(s)
+                    frontier.append(child)
+        return "Failure"
+    
+    def depth_first_search(self, problem: SearchProblem):
+        node = Node(problem.initial_state, None, 0)
+        if problem.goal_state == node.state:
+            return node
+        frontier = [node]
+        reached = [problem.initial_state]
+        while frontier:
+            n = frontier.pop()
             for child in self.expand(problem, n):
                 s = child.state
                 if problem.goal_state == s:
