1c7848fe0fa13aa38bafefc5953e886206812da2
Fausto David Hernandez Jasso <fausto.david.hernandez.jasso@ciencias.unam.mx>
2023-09-15 23:34:30 -0600

    Updating main file.

--- codigos/solving_problems_by_searching/main.py
+++ codigos/solving_problems_by_searching/main.py
@@ -4,13 +4,25 @@ from node import Node
 from algorithms import Algorithms
 from graph import Graph
 
+def solution(node: Node):
+    solution = []
+    while (node != None):
+        solution.append(node)
+        node = node.parent
+    return solution
+
+def print_solution(solution: list):
+    for node in reversed(solution):
+        print(node)
+        
+
 if __name__ == '__main__':
     problem = SearchProblem()
     alg = Algorithms()
     node = alg.breadth_first_search(problem)
-    path = []
-    while (node != None):
-        path.append(node)
-        node = node.parent
-    for node in reversed(path):
-        print(node)
+    print("BFS: ")
+    print_solution(solution(node))
+    node = alg.depth_first_search(problem)
+    print("DFS: ")
+    print_solution(solution(node))
+    
